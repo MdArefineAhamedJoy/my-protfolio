d50119fc3e695fd4b9b80edb0abddf8859a280a8
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-07-03 23:58:49 +0600

    add responsive website

--- src/SocialMedia/Socialmedia.jsx
+++ src/SocialMedia/Socialmedia.jsx
@@ -13,16 +13,16 @@ const Socialmedia = () => {
   console.log(social);
 
   return (
-    <section className="h-screen pt-10">
-      <div className=" grid grid-cols-2 px-4 text-xl">
-        <div className="flex items-center justify-center ">
+    <section className="md:h-screen h-full pt-10">
+      <div className=" grid md:grid-cols-2 px-4 text-xl">
+        <div className="flex gap-y-10 items-center md:justify-center ">
         <FaPhoneAlt size={30} /> 
         <span className="ms-10">
         <p className="py-2">O1736226388</p>
         <p>O1736226388</p>
         </span>
         </div>
-        <div className="flex items-center justify-center">
+        <div className="flex items-center md:justify-center">
         <FaMapMarkerAlt size={30} /> 
         <span className="ms-10">
         <p className="py-2">Kishorganj</p>
@@ -37,19 +37,19 @@ const Socialmedia = () => {
         <p className="text-[#ffc576]">SOCIAL MEDIA</p>
         <h2 className="text-3xl font-medium uppercase pb-10">Let’s Be Friends</h2>
       </div>
-      <div className="grid md:grid-cols-3 grid-cols-2 px-4 w-full gap-5">
+      <div className="grid md:grid-cols-3  px-4 w-full gap-5">
         {social.map(({title , subtitle , icon , link}) => (
           <div
             key={Math.random()}
-            className="flex p-7 items-center justify-between hover:bg-[#ffc576] duration-500  outline outline-2 outline-[#ffc576] rounded-sm hover:text-black hover:scale-105 "
+            className="flex p-7 items-center justify-between hover:bg-[#ffc576] duration-500  outline outline-2 outline-[#ffc576] rounded-sm hover:text-black md:hover:scale-105 "
           >
             <div>
               <h2 className="card-title uppercase">{title}</h2>
-              <small className="hidden md:block">{subtitle}</small>
+              <small className=" md:block">{subtitle}</small>
             </div>
             <div>
               <a href={link}>
-              <img src={icon} className="w-20 h-20 rounded-full" alt="" />
+              <img src={icon} className="md:w-20 md:h-20 w-16 h-16 rounded-full" alt="" />
               </a>
             </div>
           </div>
--- src/components/OthersSkills.jsx
+++ src/components/OthersSkills.jsx
@@ -1,63 +1,61 @@
-import React from 'react';
+import React from "react";
 // import img1 from "../assets/istockphoto-1413514596-170667a.webp"
 // import img2 from '../assets/istockphoto-1413514709-170667a.webp'
 // import img3 from '../assets/istockphoto-1413514774-170667a.webp'
 
 const Experience = () => {
-    // Todo: ADD Images 
-    const experiences = [
-        {
-            id:1 , 
-            title: 'Team Work',
-            style : 'shadow-orange-500'
-        },
-        {
-            id:2 , 
-            title: 'Heard Working Person',
-            style : 'shadow-pink-500'
-        },
-        {
-            id:3 , 
-            title: 'Good Communication',
-            style : 'shadow-yellow-500'
-        },
-        {
-            id:4 , 
-            title: 'Quick Learner',
-            style : 'shadow-purple-600'
-        },
-        {
-            id:5 , 
-            title: 'Flexibility,',
-            style : 'shadow-blue-400'
-        },
-        {
-            id:6 , 
-            title: 'Project Managing',
-            style : 'shadow-green-600'
-        },
-    ]
-    return (
-        <div name="experience" className=' w-full h-screen pu'>
-            <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'> 
-            <div>
-                    <p className="text-[#ffc576] text-3xl font-medium">Others Skills</p>
-              
-                </div>
-                <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
-
-            {
-                experiences.map(({id , title ,src , style})=>
-       
-                <div key={id} className={`shadow-md flex items-center justify-center hover:scale-105 duration-200 py-2 rounded-lg ${style}` }>
-                    <p className='mt-4'>{title}</p>
-                </div>)
-            }
-        
-                </div>
-            </div>
+  // Todo: ADD Images
+  const experiences = [
+    {
+      id: 1,
+      title: "Team Work",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 2,
+      title: "Heard Working Person",
+      style: "shadow-pink-500",
+    },
+    {
+      id: 3,
+      title: "Good Communication",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 4,
+      title: "Quick Learner",
+      style: "shadow-purple-600",
+    },
+    {
+      id: 5,
+      title: "Flexibility,",
+      style: "shadow-blue-400",
+    },
+    {
+      id: 6,
+      title: "Project Managing",
+      style: "shadow-green-600",
+    },
+  ];
+  return (
+    <div name="experience" className=" w-full h-screen pu">
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+        <div>
+          <p className="text-[#ffc576] text-3xl font-medium">Others Skills</p>
         </div>
-    );
+        <div className="w-full grid  sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+          {experiences.map(({ id, title, src, style }) => (
+            <div
+              key={id}
+              className={`shadow-md flex items-center justify-center hover:scale-105 duration-200 py-2 rounded-lg ${style}`}
+            >
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Experience;
